2d5fd177dab223bf2ea6ef293ffac3379f43d9cf
oldmtn <oldmtn@qq.com>
2018-02-06 13:05:42 +0800

    update code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-﻿import Events from './core/CoreEvents';
+﻿// 
+import Events from './core/CoreEvents';
+import CommonUtils from './utils/common_utils';
 import Player from './Player';
-import TestFeature from './TestFeature';
 import CastSender from './cast/cast_sender';
 import CastReceiver from './cast/cast_receiver';
 
-import CommonUtils from './utils/common_utils';
+//
+import TestFeature from './TestFeature';
 
 // Shove both of these into the global scope
 var context = (typeof window !== 'undefined' && window) || global;
