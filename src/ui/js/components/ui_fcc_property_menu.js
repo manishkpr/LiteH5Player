feede05f25392e5491c7891710839c1dee8ca445
oldmtn <oldmtn@qq.com>
2018-06-29 19:42:58 +0800

    Remove unnecessary import declaration.

--- src/ui/js/components/ui_fcc_property_menu.js
+++ src/ui/js/components/ui_fcc_property_menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 class UIFccPropertyMenu extends React.Component {
   constructor(props) {
--- src/ui/js/components/ui_subtitles_menu.js
+++ src/ui/js/components/ui_subtitles_menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 class UISubtitlesMenu extends React.Component {
   constructor(props) {
